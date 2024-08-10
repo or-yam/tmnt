d5bcea54b2edce5b02d59cf74748d6c7bb948b13
Oryam <48219965+or-yam@users.noreply.github.com>
2024-08-10 22:17:28 +0300

    Add sounds

---
+++ src/audio.ts
@@ -0,0 +1,21 @@
+export const audioInit = () => {
+  const selectSound = new Audio('sounds/select.mp3');
+  selectSound.volume = 0.2;
+  const backgroundMusic = new Audio('sounds/background.mp3');
+  backgroundMusic.loop = true;
+
+  const playBackgroundMusic = () => {
+    if (backgroundMusic.paused) {
+      backgroundMusic.play();
+    }
+  };
+
+  const playSelectSound = () => {
+    selectSound.play();
+  };
+
+  return {
+    playBackgroundMusic,
+    playSelectSound
+  };
+};
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { audioInit } from './audio';
 import { gamepadInit, checkGamepads } from './gamepad';
 import './style.css';
 
@@ -7,7 +8,11 @@ let currentTurtleIndex = 0;
 
 gamepadInit();
 
+const { playBackgroundMusic, playSelectSound } = audioInit();
+
 const moveRight = () => {
+  playBackgroundMusic();
+  playSelectSound();
   turtles.item(currentTurtleIndex).classList.remove('selected');
   currentTurtleIndex = currentTurtleIndex >= turtles.length - 1 ? 0 : currentTurtleIndex + 1;
   turtles.item(currentTurtleIndex).classList.add('selected');
@@ -15,6 +20,8 @@ const moveRight = () => {
 };
 
 const moveLeft = () => {
+  playBackgroundMusic();
+  playSelectSound();
   turtles.item(currentTurtleIndex).classList.remove('selected');
   currentTurtleIndex = currentTurtleIndex <= 0 ? turtles.length - 1 : currentTurtleIndex - 1;
   turtles.item(currentTurtleIndex).classList.add('selected');
